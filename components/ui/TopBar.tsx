e00080d506b382151dccdc953800165ecfc6be7e
Azumi Shimojo <azumishimojo@Azu-Mac.local>
2025-01-21 11:08:20 +0000

    add-logo-kana

--- components/ui/TopBar.tsx
+++ components/ui/TopBar.tsx
@@ -16,7 +16,12 @@ const TopBar = () => {
         className="text-2xl font-semibold cursor-pointer hover:cursor-pointer"
         onClick={navigateHome}
       >
-        和 Ryokan
+        <img
+        src="/assets/logo.png" // Path to your logo
+        alt="Logo"
+        className="w-8 h-8 mr-2"
+        />
+        和 旅館
       </div>
       <ul className="flex items-center space-x-6">
         <li>
